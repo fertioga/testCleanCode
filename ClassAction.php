70857853afb35d7d58d37c12821d0001850e6b65
Fernando França <fertioga@gmail.com>
2022-03-10 14:35:55 -0300

    first Commit

---
+++ ClassAction.php
@@ -0,0 +1,36 @@
+<?php
+
+class Action
+{
+	public function doSomething($param1,$param2,$param3,$param4,$param5,$param6)
+	{
+        $return['calc_factor'] = ($param1 + $param3 + $param4 + $param5) * $param6;
+
+        $return['vlr_type'] = "";
+
+        for($c=0; $c < count($param2); $c++ )
+        {
+            for($d=0; $d<count($param2[$c]); $d++)
+            {
+                if($param2[$c]['vlr'] == 0.00)
+                {
+                    $vlr_type = "FREE";
+                }
+                elseif($param2[$c]['vlr'] >= 0.01 and $param2[$c]['vlr'] <= 10)
+                {
+                    $vlr_type = "CHEAP";
+                }
+                elseif($param2[$c]['vlr'] > 10 and $param2[$c]['vlr'] <= 20)
+                {
+                    $vlr_type = "GOOD_VALUE";
+                }
+                elseif($param2[$c]['vlr'] > 20)
+                {
+                    $vlr_type = "EXPENSIVE";
+                }
+            }
+        }
+
+        return $return;
+	}
+}
